bc83ba3864fd562b3f72434e646df4688abff6e0
brahanlider <brahanlidertunquipamamani@gmail.com>
2025-03-29 10:45:46 -0500

    arrastrar tasks por Id with zustand

--- src/components/jira/SingleTask.tsx
+++ src/components/jira/SingleTask.tsx
@@ -1,13 +1,24 @@
 import { IoReorderTwoOutline } from "react-icons/io5";
 import { Task } from "../../interfaces";
+import { useTaskStore } from "../../store";
 
 interface Props {
   task: Task;
 }
 
 export const SingleTask = ({ task }: Props) => {
+  const setDraggingTaksId = useTaskStore((state) => state.setDraggingTaksId);
+  const removeDragginTaskId = useTaskStore(
+    (state) => state.removeDragginTaskId
+  );
+
   return (
-    <div className="flex items-center justify-between p-2 mt-5">
+    <div
+      draggable
+      onDragStart={() => setDraggingTaksId(task.id)}
+      onDragEnd={() => removeDragginTaskId()}
+      className="flex items-center justify-between p-2 mt-5"
+    >
       <div className="flex items-center justify-center gap-2">
         <p className="text-base font-bold text-navy-700">{task.title}</p>
       </div>
--- src/store/tasks/task.storage.ts
+++ src/store/tasks/task.storage.ts
@@ -1,17 +1,22 @@
 import { create, StateCreator } from "zustand";
 import type { Task, TaskStatus } from "../../interfaces";
-// import { devtools, persist } from "zustand/middleware";
+import { devtools } from "zustand/middleware";
 
 interface TaskState {
+  draggingTaskId?: string;
   tasks: Record<string, Task>; //{[key:string]:Task}
 
   getTaskByStatus: (status: TaskStatus) => Task[];
+
+  setDraggingTaksId: (taskId: string) => void;
+  removeDragginTaskId: () => void;
 }
 
 // interface Actions {
 // }
 
 const storeApi: StateCreator<TaskState> = (set, get) => ({
+  draggingTaskId: undefined,
   tasks: {
     "ABC-1": { id: "ABC-1", title: "Task 1", status: "open" },
     "ABC-2": { id: "ABC-2", title: "Task 2", status: "done" },
@@ -26,15 +31,21 @@ const storeApi: StateCreator<TaskState> = (set, get) => ({
 
     return Object.values(get().tasks).filter((task) => task.status === status);
   },
+  setDraggingTaksId: (taskId: string) => {
+    set({ draggingTaskId: taskId });
+  },
+  removeDragginTaskId: () => {
+    set({ draggingTaskId: undefined });
+  },
 });
 
 export const useTaskStore = create<TaskState>()(
-  storeApi
-  // // devtools(
-  // // persist(
-  // (...a) => ({
-  //   ...storeApi(...a),
-  // })
-  // // )
-  // // )
+  devtools(
+    storeApi
+    // // persist(
+    // (...a) => ({
+    //   ...storeApi(...a),
+    // })
+    // // )
+  )
 );
