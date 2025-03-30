4cb6ef07f192b3ba9e0f18f33227b0cf2387041e
brahanlider <brahanlidertunquipamamani@gmail.com>
2025-03-29 23:41:12 -0500

    Store and Slice => useGuest WITH Consumiendo WeddingPage

---
+++ src/store/wedding/guest.slice.ts
@@ -0,0 +1,22 @@
+import { StateCreator } from "zustand";
+
+export interface GuestSlice {
+  guestCount: number;
+
+  setGuestCount: (guestCount: number) => void;
+}
+
+export const createGuestSlice: StateCreator<
+  GuestSlice,
+  [["zustand/devtools", never]]
+> = (set) => ({
+  guestCount: 0,
+  
+  setGuestCount: (guestCount: number) => {
+    set(
+      { guestCount: guestCount < 0 ? 0 : guestCount },
+      false,
+      "setGuestCount"
+    );
+  },
+});
--- src/store/wedding/index.ts
+++ src/store/wedding/index.ts
@@ -1,13 +1,15 @@
 import { create } from "zustand";
 import { createPersonSlice, PersonSlice } from "./person.slice";
 import { devtools } from "zustand/middleware";
+import { createGuestSlice, GuestSlice } from "./guest.slice";
 
 // Crear el store
 
-type ShareState = PersonSlice;
+type ShareState = PersonSlice & GuestSlice;
 
 export const useWeddingBoundStore = create<ShareState>()(
   devtools((...a) => ({
     ...createPersonSlice(...a),
+    ...createGuestSlice(...a),
   }))
 );
